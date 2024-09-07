29f79899980181988cef42ca1b0f64007d6923be
f.b.nascimento <f.b.nascimento@avanade.com>
2024-09-07 00:14:24 -0300

    ..

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { getInput } from '@actions/core';
 
 import { extractVariables } from './scripts/extract_var_yaml_to_env';
 
-const yamlFile = getInput('yaml_file');
+const yamlFile = getInput('config_file');
 const prefix = getInput('prefix') || 'YAML_';
 
 extractVariables(yamlFile, prefix);
